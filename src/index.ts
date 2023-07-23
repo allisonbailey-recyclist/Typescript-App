f162c188d880e45e4083e1334909ba7ef6ee91bb
Allison Bailey <abailey@routeware.com>
2023-07-22 19:14:35 -0700

    installed package uuid and added import to index.ts file.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+// import { v4 as uuidV4 } from 'uuid'
 
+// console.log(uuidV4())
 import confetti from 'canvas-confetti';
 
 confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
